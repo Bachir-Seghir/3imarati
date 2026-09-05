77d399bdd799b2471652723c0165a42064dcf211
bachir-seghir <bachir.seghir17@gmail.com>
2026-09-06 00:22:38 +0100

    register scroll fix and icon change

--- app/auth/register.tsx
+++ app/auth/register.tsx
@@ -6,7 +6,15 @@ import { Link, router } from "expo-router";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { doc, serverTimestamp, setDoc } from "firebase/firestore";
 import { useState } from "react";
-import { Image, Pressable, Text, TextInput, View } from "react-native";
+import {
+	Image,
+	Platform,
+	Pressable,
+	ScrollView,
+	Text,
+	TextInput,
+	View,
+} from "react-native";
 
 export default function RegisterScreen() {
 	const [email, setEmail] = useState("");
@@ -109,97 +117,105 @@ export default function RegisterScreen() {
 
 	return (
 		<Screen>
-			<View className="mt-4 p-4  justify-center">
-				<Text className="text-3xl mx-auto font-bold mb-4">
-					Créer un nouveau compte
-				</Text>
-
-				<TextInput
-					placeholder="Email"
-					className="border p-3 rounded-md mb-3"
-					onChangeText={setEmail}
-				/>
-				<TextInput
-					placeholder="0770xxx"
-					keyboardType="phone-pad"
-					className="border p-3 rounded-md mb-3"
-					onChangeText={setPhone}
-				/>
-
-				<TextInput
-					placeholder="Mot de Pass"
-					secureTextEntry
-					className="border p-3 rounded-md mb-3"
-					onChangeText={setPassword}
-				/>
-
-				<TextInput
-					placeholder="Nom & Prénom"
-					className="border p-3 rounded-md mb-3"
-					onChangeText={setFullName}
-				/>
-
-				<TextInput
-					placeholder="Étage Ex: 3"
-					keyboardType="numeric"
-					className="border p-3 rounded-md mb-3"
-					onChangeText={setFloor}
-				/>
-
-				<TextInput
-					placeholder="Porte Ex: 5"
-					keyboardType="numeric"
-					className="border p-3 rounded-md mb-3"
-					onChangeText={setDoor}
-				/>
-
-				{/* Image picker */}
-				<Text className="font-semibold mb-2">
-					Photo du CIN d'acquéreur ou Locataire
-				</Text>
-				<View className="flex-row gap-2 mb-6">
-					<Pressable
-						onPress={takePhoto}
-						className="bg-blue-600 p-3 rounded-md flex-1"
-					>
-						<Text className="text-white text-center font-semibold">
-							Prendre photo
-						</Text>
-					</Pressable>
-
-					<Pressable
-						onPress={pickImage}
-						className="bg-gray-600 p-3 rounded-md flex-1"
-					>
-						<Text className="text-white text-center font-semibold">
-							Galerie
-						</Text>
-					</Pressable>
-				</View>
-
-				{/* Preview */}
-				{image && (
-					<Image
-						source={{ uri: image }}
-						className="w-full h-52 rounded-md mb-4"
-						resizeMode="cover"
-					/>
-				)}
-				<Pressable
-					onPress={handleRegister}
-					className="bg-blue-600 p-3 rounded"
-				>
-					<Text className="text-white text-center">Inscrir</Text>
-				</Pressable>
-				<Link
-					href="/auth/login"
-					asChild
-				>
-					<Text className="text-blue-600 mt-4 text-center">
-						J'ai déja un Compte
+			<ScrollView
+				className="flex-1 px-4 pt-10"
+				contentContainerStyle={{
+					paddingBottom: Platform.OS === "ios" ? 60 : 100,
+					gap: 20,
+				}}
+			>
+				<View className="mt-4 p-4  justify-center">
+					<Text className="text-3xl mx-auto font-bold mb-4">
+						Créer un nouveau compte
 					</Text>
-				</Link>
-			</View>
+
+					<TextInput
+						placeholder="Email"
+						className="border p-3 rounded-md mb-3"
+						onChangeText={setEmail}
+					/>
+					<TextInput
+						placeholder="0770xxx"
+						keyboardType="phone-pad"
+						className="border p-3 rounded-md mb-3"
+						onChangeText={setPhone}
+					/>
+
+					<TextInput
+						placeholder="Mot de Pass"
+						secureTextEntry
+						className="border p-3 rounded-md mb-3"
+						onChangeText={setPassword}
+					/>
+
+					<TextInput
+						placeholder="Nom & Prénom"
+						className="border p-3 rounded-md mb-3"
+						onChangeText={setFullName}
+					/>
+
+					<TextInput
+						placeholder="Étage Ex: 3"
+						keyboardType="numeric"
+						className="border p-3 rounded-md mb-3"
+						onChangeText={setFloor}
+					/>
+
+					<TextInput
+						placeholder="Porte Ex: 5"
+						keyboardType="numeric"
+						className="border p-3 rounded-md mb-3"
+						onChangeText={setDoor}
+					/>
+
+					{/* Image picker */}
+					<Text className="font-semibold mb-2">
+						Photo du CIN d'acquéreur ou Locataire
+					</Text>
+					<View className="flex-row gap-2 mb-6">
+						<Pressable
+							onPress={takePhoto}
+							className="bg-blue-600 p-3 rounded-md flex-1"
+						>
+							<Text className="text-white text-center font-semibold">
+								Prendre photo
+							</Text>
+						</Pressable>
+
+						<Pressable
+							onPress={pickImage}
+							className="bg-gray-600 p-3 rounded-md flex-1"
+						>
+							<Text className="text-white text-center font-semibold">
+								Galerie
+							</Text>
+						</Pressable>
+					</View>
+
+					{/* Preview */}
+					{image && (
+						<Image
+							source={{ uri: image }}
+							className="w-full h-52 rounded-md mb-4"
+							resizeMode="cover"
+						/>
+					)}
+					<Pressable
+						onPress={handleRegister}
+						className="bg-blue-600 p-3 rounded"
+					>
+						<Text className="text-white text-center">Inscrir</Text>
+					</Pressable>
+					<Link
+						href="/auth/login"
+						asChild
+					>
+						<Text className="text-blue-600 mt-4 text-center">
+							J'ai déja un Compte
+						</Text>
+					</Link>
+				</View>
+			</ScrollView>
 		</Screen>
 	);
 }
